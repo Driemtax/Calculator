9743e11f0e0caf2bf0c680122628ecc55e6de9e5
Lars Beer <driemtax@gmail.com>
2025-07-19 20:23:39 +0200

    feat: adding user input

--- cmd/calculator/main.go
+++ cmd/calculator/main.go
@@ -1,17 +1,35 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/driemtax/Calculator/pkg/calculator"
 )
 
 func main() {
-	var input = "1 + 2 * 3"
-	var result, err = calculator.Evaluate(input)
-	if err != nil {
-		fmt.Println("Error: ", err)
-	} else {
-		fmt.Println("Result: ", result)
+	fmt.Print("$ Welcome! To exit just type exit\n")
+	for {
+		// Getting input from user
+		fmt.Print("$ ")
+		input, inputErr := bufio.NewReader(os.Stdin).ReadString('\n')
+		if inputErr != nil {
+			fmt.Println("Error reading input:", inputErr)
+		}
+
+		input = strings.TrimSpace(input)
+
+		if input == "exit" {
+			os.Exit(0)
+		}
+		// calculate and display the result
+		var result, err = calculator.Evaluate(input)
+		if err != nil {
+			fmt.Println("Error:", err)
+		} else {
+			fmt.Println("$ Result:", result)
+		}
 	}
 }
