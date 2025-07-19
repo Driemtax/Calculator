959fa6191361a13903a9241450bed4ba803b57c2
Lars Beer <driemtax@gmail.com>
2025-07-19 18:31:26 +0200

    feat: working tokenizer

--- cmd/calculator/main.go
+++ cmd/calculator/main.go
@@ -4,10 +4,11 @@ import (
 	"fmt"
 
 	"github.com/driemtax/Calculator/pkg/calculator"
+	"github.com/driemtax/Calculator/pkg/scanner"
 )
 
 func main() {
-	var input = "2+3"
+	var input = "(1 + 2) * 3"
 	var result, err = calculator.Evaluate(input)
 	if err != nil {
 		fmt.Println("Error: ", err)
@@ -15,4 +16,9 @@ func main() {
 		fmt.Println("Result: ", result)
 	}
 
+	var tokenList = scanner.Tokenize(input)
+
+	for i, token := range tokenList {
+		fmt.Println("Token ", i, ": ", token)
+	}
 }
---
+++ pkg/scanner/tokenizer.go
@@ -0,0 +1,24 @@
+package scanner
+
+import (
+	"regexp"
+)
+
+// Tokenize breaks down a mathematical expression string into individual tokens.
+// It recognizes numbers (integers), arithmetic operators (+, -, *, /),
+// parentheses, and filters out whitespace and tabs.
+//
+// The function returns a slice of strings where each string represents
+// a single token from the input expression.
+//
+// Example:
+//
+//	tokens := Tokenize("2 + 3 * (4 - 1)")
+//	// Returns: ["2", "+", "3", "*", "(", "4", "-", "1", ")"]
+func Tokenize(expression string) []string {
+	var tokenList = []string{}
+	re := regexp.MustCompile(`\d+|[+\-*/()]`)
+	tokenList = re.FindAllString(expression, -1)
+
+	return tokenList
+}
