990d3bd46459e6e69a7b1e59dc32dbbd5a76215f
Lars Beer <driemtax@gmail.com>
2025-07-21 15:02:26 +0200

    feat: not completly working parser for sin, cos and tan

--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -18,6 +18,8 @@ package parser
 
 import (
 	"errors"
+	"fmt"
+	"math"
 	"slices"
 	"strconv"
 
@@ -27,12 +29,44 @@ import (
 // Parse parses a slice of tokens representing a mathematical expression and returns
 // the evaluated result as a float64. It ensures all tokens are consumed during parsing.
 func Parse(tokens []string) (float64, error) {
-	result, tokenRest, err := parseExpression(tokens)
+	result, tokenRest, err := parseFunction(tokens)
 	if len(tokenRest) != 0 {
 		err = errors.Join(err, errors.New("still tokens left"))
 	}
 
 	return result, err
+}
+
+func parseFunction(tokens []string) (float64, []string, error) {
+	operators := []string{"sin", "cos", "tan"}
+	var result float64
+	var tokenRest []string
+	var err error
+
+	if slices.Contains(operators, tokens[0]) {
+		if tokens[1] != "(" {
+			err = errors.New("did you miss a '('?")
+			return result, tokenRest, err
+		}
+		fmt.Printf("Before iterating with: %q\n", tokens)
+		result, tokenRest, err = parseExpression(tokens[1:])
+
+		fmt.Printf("Result: %2f, TokenRest: %q\n", result, tokenRest)
+		// now calculate the actual sin,cos, or tan of the result
+		switch tokens[0] {
+		case "sin":
+			result = math.Sin(result)
+		case "cos":
+			result = math.Cos(result)
+		case "tan":
+			result = math.Tan(result)
+		}
+	} else {
+		fmt.Printf("Function again: %q\n", tokens)
+		result, tokenRest, err = parseExpression(tokens)
+	}
+
+	return result, tokenRest, err
 }
 
 func parseExpression(tokens []string) (float64, []string, error) {
@@ -73,19 +107,31 @@ func parseProduct(tokens []string) (float64, []string, error) {
 }
 
 func parseFactor(tokens []string) (float64, []string, error) {
+	operators := []string{"sin", "cos", "tan"}
 	result := 0.0
 	var err error = nil
 	num := false
 	tokenRest := tokens
 
 	if tokenRest[0] == "(" {
-		result, tokenRest, err = parseExpression(tokens[1:])
+		fmt.Printf("Factor: %q\n", tokenRest)
+		result, tokenRest, err = parseFunction(tokens[1:])
+		fmt.Printf("After Function: %2f, %q\n", result, tokenRest)
+		if len(tokenRest) == 0 {
+			return result, tokenRest, err
+		}
 		if !(tokenRest[0] == ")") {
 			errorMessage := "')' expexted, got " + tokenRest[0]
 			err = errors.New(errorMessage)
 			return result, tokenRest, err
 		}
+	} else if slices.Contains(operators, tokenRest[0]) {
+		result, tokenRest, err = parseFunction(tokens)
+		if len(tokenRest) == 0 {
+			return result, tokenRest, err
+		}
 	} else {
+		fmt.Printf("Number: %s\n", tokenRest[0])
 		result, num = isNumber(tokenRest[0])
 		if !(num) {
 			errorMessage := "Number expected, got " + tokenRest[0]
@@ -94,6 +140,7 @@ func parseFactor(tokens []string) (float64, []string, error) {
 		}
 	}
 
+	fmt.Printf("Returning: %2f, %q\n", result, tokenRest)
 	return result, tokenRest[1:], err
 }
 
